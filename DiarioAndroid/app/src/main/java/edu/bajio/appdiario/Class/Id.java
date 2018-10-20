4d074e4df8d70ca5416e046bbca38999309584ee
Luis Fernando Vázquez Villaseñor <luis.vavi.96@gmail.com>
2018-10-20 14:44:46 -0500

    Actualizacion Interfaces y configuracion

---
+++ DiarioAndroid/app/src/main/java/edu/bajio/appdiario/Class/Id.java
@@ -0,0 +1,16 @@
+package edu.bajio.appdiario.Class;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Id {
+    @SerializedName("$oid")
+    private String oid;
+
+    public String getOid() {
+        return oid;
+    }
+
+    public void setOid(String oid) {
+        this.oid = oid;
+    }
+}
